fec5ae3682f079ed4493db92fa134bbf7b8841c9
beming-dev <mingfordev@gmail.com>
2023-04-01 14:14:32 +0900

    Add adsense

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,6 +14,8 @@ const IndexPage = ({ data }) => (
         name="google-site-verification"
         content="3IsW427y9ifgDvg2qMuZiSL9WStHw0YN-C57cwpHjrE"
       />
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4911380706120456"
+     crossorigin="anonymous"></script>
     </Helmet>
     <Seo title="Home" />
     <div className="main">
