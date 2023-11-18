030b02e0832d778aa339c0579d9ffec1344dc013
beming-dev <mingfordev@gmail.com>
2023-11-18 20:17:39 +0100

    Add table for post

--- src/pages/{MarkdownRemark.frontmatter__slug}.js
+++ src/pages/{MarkdownRemark.frontmatter__slug}.js
@@ -5,17 +5,24 @@ import Seo from "../components/seo"
 import "../styles/post.scss"
 
 export default function BlogPostTemplate({ data: { markdownRemark } }) {
-  const { frontmatter, html } = markdownRemark
+  const { frontmatter, html, tableOfContents } = markdownRemark
+  console.log(tableOfContents)
   return (
     <Layout>
       <Seo title={frontmatter.title} />
-      <div className="post-page">
-        <h1 className="title">{frontmatter.title}</h1>
-        <h2 className="date">{frontmatter.date}</h2>
-        <span
-          className="post-body"
-          dangerouslySetInnerHTML={{ __html: html }}
-        ></span>
+      <div className="post-box">
+        <div className="post-page">
+          <h1 className="title">{frontmatter.title}</h1>
+          <h2 className="date">{frontmatter.date}</h2>
+          <span
+            className="post-body"
+            dangerouslySetInnerHTML={{ __html: html }}
+          ></span>
+        </div>
+        <div
+          className="table"
+          dangerouslySetInnerHTML={{ __html: tableOfContents }}
+        />
       </div>
     </Layout>
   )
@@ -29,6 +36,7 @@ export const pageQuery = graphql`
         date(formatString: "MMMM DD, YYYY")
         title
       }
+      tableOfContents
     }
   }
 `
