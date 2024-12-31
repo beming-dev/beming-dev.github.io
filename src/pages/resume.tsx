dda64fc6d0c799c6f7ede4745734b94b13bfd294
beming-dev <mingfordev@gmail.com>
2024-12-31 12:50:15 +0900

    Add post

--- src/pages/resume.tsx
+++ src/pages/resume.tsx
@@ -36,7 +36,7 @@ const ResumePage = () => {
           <div>
             <div className="self-start gap-3 flex justify-between items-center">
               {/* 이름 & 직책 */}
-              <div>
+              <div className="min-h-full  flex flex-col justify-between">
                 <h1 className="text-2xl font-bold mb-2">
                   채민관 (Chae Min Gwan)
                 </h1>
@@ -119,15 +119,16 @@ const ResumePage = () => {
                 </h3>
                 <p className="text-sm text-gray-500 mb-2">
                   {lang === "ko"
-                    ? "2019.01 ~ 2021.12 | 프론트엔드 개발자"
+                    ? "2022.06 ~ * | 백엔드 개발자"
                     : "Jan 2019 ~ Dec 2021 | Frontend Developer"}
                 </p>
-                <ul className="list-disc list-inside text-sm text-gray-700 space-y-1">
+                <ul className="list-disc list-inside text-sm text-gray-700 space-y-1 mt-2">
                   {lang === "ko" ? (
                     <>
-                      <li>React.js 웹 애플리케이션 개발</li>
-                      <li>UI/UX 개선 및 반응형 웹 구현</li>
-                      <li>코드 리뷰 및 팀원 멘토링 지원</li>
+                      <li>Express.js, Nest.js, MongoDB</li>
+                      <li>스터디 매칭을 위한 클러스터링 알고리즘 구현</li>
+                      <li>비용 최적화를 위해 AWS에 CI/CD 파이프라인 구축</li>
+                      <li>MongoDB 최적화</li>
                     </>
                   ) : (
                     <>
@@ -146,15 +147,14 @@ const ResumePage = () => {
                 </h3>
                 <p className="text-sm text-gray-500 mb-2">
                   {lang === "ko"
-                    ? "2019.01 ~ 2021.12 | 프론트엔드 개발자"
+                    ? "2024.11 ~ 2024.11 | 프론트엔드 개발자"
                     : "Jan 2019 ~ Dec 2021 | Frontend Developer"}
                 </p>
-                <ul className="list-disc list-inside text-sm text-gray-700 space-y-1">
+                <ul className="list-disc list-inside text-sm text-gray-700 space-y-1  mt-2">
                   {lang === "ko" ? (
                     <>
-                      <li>React.js 웹 애플리케이션 개발</li>
-                      <li>UI/UX 개선 및 반응형 웹 구현</li>
-                      <li>코드 리뷰 및 팀원 멘토링 지원</li>
+                      <li>Next.js, Tailwindcss</li>
+                      <li>디자이너와의 협업으로 UI/UX 개선</li>
                     </>
                   ) : (
                     <>
@@ -181,15 +181,18 @@ const ResumePage = () => {
                 </h3>
                 <p className="text-sm text-gray-500 mb-2">
                   {lang === "ko"
-                    ? "2019.01 ~ 2021.12 | 프론트엔드 개발자"
+                    ? "2024.12 ~ * | 풀스택 개발"
                     : "Jan 2019 ~ Dec 2021 | Frontend Developer"}
                 </p>
-                <ul className="list-disc list-inside text-sm text-gray-700 space-y-1">
+                <ul className="list-disc list-inside text-sm text-gray-700 space-y-1  mt-2">
                   {lang === "ko" ? (
                     <>
-                      <li>React.js 웹 애플리케이션 개발</li>
-                      <li>UI/UX 개선 및 반응형 웹 구현</li>
-                      <li>코드 리뷰 및 팀원 멘토링 지원</li>
+                      <li>Spring, MySQL, React.js, tailwindCSS</li>
+                      <li>
+                        Oracle cloud와 Jenkins를 이용한 CI/CD 파이프라인 구축
+                      </li>
+                      <li>Redis, Kafka를 이용한 대용량 데이터 처리</li>
+                      <li>MySQL query 최적화</li>
                     </>
                   ) : (
                     <>
@@ -211,21 +214,21 @@ const ResumePage = () => {
             <div className="bg-white shadow-sm border border-gray-200 rounded-md p-4">
               <div className="flex flex-wrap gap-2">
                 {/* 각각의 박스 */}
-                <div className="w-full flex justify-between">
-                  <div className="flex flex-col justify-start items-center space-y-2">
-                    <span>Frontend</span>
+                <div className="w-full flex flex-col justify-between space-y-2">
+                  <div className="flex items-center space-x-2">
+                    <span className="self-center w-24">Frontend</span>
+                    <span className="px-3 py-1 text-xs rounded-full bg-indigo-100 text-indigo-600 mt-0">
+                      Tailwind.css
+                    </span>
                     <span className="px-3 py-1 text-xs rounded-full bg-indigo-100 text-indigo-600">
                       React.js
                     </span>
                     <span className="px-3 py-1 text-xs rounded-full bg-indigo-100 text-indigo-600">
                       Next.js
                     </span>
-                    <span className="px-3 py-1 text-xs rounded-full bg-indigo-100 text-indigo-600">
-                      Tailwind.css
-                    </span>
                   </div>
-                  <div className="flex flex-col justify-start items-center space-y-2">
-                    <span>Backend</span>
+                  <div className="flex items-center space-x-2">
+                    <span className="self-center w-24">Backend</span>
                     <span className="px-3 py-1 text-xs rounded-full bg-indigo-100 text-indigo-600">
                       Express.js
                     </span>
@@ -236,17 +239,39 @@ const ResumePage = () => {
                       Spring
                     </span>
                   </div>
-                  <div className="flex flex-col justify-start items-center space-y-2">
-                    <span>Etc.</span>
+                  <div className="flex items-center space-x-2">
+                    <span className="self-center w-24">Database</span>
                     <span className="px-3 py-1 text-xs rounded-full bg-indigo-100 text-indigo-600">
-                      Git
+                      MySQL
                     </span>
                     <span className="px-3 py-1 text-xs rounded-full bg-indigo-100 text-indigo-600">
+                      MongoDB
+                    </span>
+                  </div>
+                  <div className="flex items-center space-x-2">
+                    <span className="self-center w-24">Etc.</span>
+                    <span className="px-3 py-1 text-xs rounded-full bg-indigo-100 text-indigo-600">
                       Docker
+                    </span>
+                    <span className="px-3 py-1 text-xs rounded-full bg-indigo-100 text-indigo-600">
+                      Git
                     </span>
                   </div>
                 </div>
               </div>
+            </div>
+          </section>
+
+          {/* 공부중인것 섹션 */}
+          <section className="mb-8">
+            <h2 className="text-lg font-semibold mb-2 border-b border-gray-300 pb-1">
+              {lang === "ko" ? "공부중인것" : "Studying"}
+            </h2>
+            <div className="bg-white shadow-sm border border-gray-200 rounded-md p-4">
+              <ul className="text-sm text-gray-700">
+                <li className="text-black text-md">Redis</li>
+                <li className="text-black text-md">Kafka</li>
+              </ul>
             </div>
           </section>
 
@@ -256,12 +281,33 @@ const ResumePage = () => {
               {lang === "ko" ? "기타" : "Etc."}
             </h2>
             <div className="bg-white shadow-sm border border-gray-200 rounded-md p-4">
-              <p className="text-sm text-gray-700">
-                {lang === "ko"
-                  ? "자격증, 수상 내역, 동아리 활동, 개인 프로젝트, 취미 등…"
-                  : "Certificates, awards, club activities, personal projects, hobbies..."}
-              </p>
+              <h3 className="font-semibold text-base">
+                {lang === "ko" ? "교환학생" : "Exchange Student"} | The Hague
+                University of Applied Sciences
+              </h3>
+              <p className="text-sm text-gray-500">2023.08 ~ 2024.02</p>
+              <ul className="text-sm text-gray-700 mt-2">
+                <li>Becoming the Next successful online startup 수업 수강</li>
+                <li>
+                  온라인 스타트업을 시작하기 위해 거쳐야하는 과정들을 직접
+                  서비스를 제작하며 경험
+                </li>
+                <li>
+                  Next.js와 Next.js api를 이용해 헬스케어 온라인 스타트업
+                  프로토타입 개발
+                </li>
+              </ul>
             </div>
+            <div className="bg-white shadow-sm border border-gray-200 rounded-md p-4">
+              <h3 className="font-semibold text-base">TOEIC (875/990)</h3>
+              <p className="text-sm text-gray-500">2024.02</p>
+            </div>
+            {/* <div className="bg-white shadow-sm border border-gray-200 rounded-md p-4">
+              <h3 className="font-semibold text-base">
+                {lang === "ko" ? "정보처리기사" : "Exchange Student"}
+              </h3>
+              <p className="text-sm text-gray-500">2025.02</p>
+            </div> */}
           </section>
         </div>
       </div>
